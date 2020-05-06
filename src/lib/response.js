e6247a94917ee8da442f1c98c4ff9af46e0926ca
Luis Adrián Chávez Fregoso <biolacf@gmail.com>
2020-05-05 21:10:21 -0500

    Solves CORS issues

--- src/lib/response.js
+++ src/lib/response.js
@@ -1,5 +1,9 @@
 module.exports = (message, statusCode = 200) => ({
   statusCode: 200,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Credentials': true,
+  },
   body: JSON.stringify({
     message,
     statusCode,
